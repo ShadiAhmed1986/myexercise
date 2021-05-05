717f73e540a0e7d111786579d74f3ac8a1cefba7
Shadi Ahmed <79768622+ShadiAhmed1986@users.noreply.github.com>
2021-05-05 19:49:49 +0100

    one question

--- src/myPractice04_05/FindUniqueElement.java
+++ src/myPractice04_05/FindUniqueElement.java
@@ -7,30 +7,41 @@ public class FindUniqueElement {
 	
 	/*
 	   Create a method to return just non-repeated element or elements
-	 	[3, 2, 5, 5, 1, 3, 1] ==> [2]
-	 	[3, 2, 5, 5, 7, 3, 1] ==> [2, 7, 1]
+
+	 	int arr [] = {3, 2, 5, 5, 7, 3, 1} ==> 2, 7, 1
+	 				 {3, 2, 5, 5, 1, 3, 1} ==> 2
 	 */
 
 	public static void main(String[] args) {
 		
 		int arr [] = {3, 2, 5, 5, 7, 3, 1};
 		
-		System.out.println(findUniqueEl(arr));
-	}
-	
-	public static List<Integer> findUniqueEl(int arr[]) {
-		
-		List<Integer>list = new ArrayList<>();
+		List<Integer>list1 = new ArrayList<>();
 		
 		for (int i = 0; i < arr.length; i++) {
-			for (int j = 0; j < arr.length; j++) {
-				if(arr[i] != arr[j] && !list.contains(arr[i])) {
-					list.add(arr[i]);
+			if(!list1.contains(arr[i])) {
+				list1.add(arr[i]);
+			}
+		}
+		
+		System.out.println(list1);
+		
+		List<Integer>list2 = new ArrayList<>();
+		
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = i+1; j < arr.length; j++) {
+				if(arr[i] == arr[j]) {
+					list2.add(arr[i]);
 				}
 			}
 		}
 		
-		return list;
+		System.out.println(list2);
+		
+		list1.removeAll(list2);
+		
+		System.out.println(list1);
+
 	}
 
 }
