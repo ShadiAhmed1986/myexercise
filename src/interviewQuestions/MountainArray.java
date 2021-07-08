70ac6d7996467a2c93256a107b5aa867ce33136d
Shadi Ahmed <79768622+ShadiAhmed1986@users.noreply.github.com>
2021-07-08 09:38:29 +0100

    repeated chars

---
+++ src/interviewQuestions/MountainArray.java
@@ -0,0 +1,15 @@
+package interviewQuestions;
+
+public class MountainArray {
+	
+	/*
+	Write java code that checks if an array is mountain array or not.
+		Mountain Array ==> [0,2,5,3,1]
+	It is constantly increasing to the maximum value,
+	decreasing constantly after the maximum value.
+		Not Mountain Array ==> [5,2,7,1,4]
+	it is decreasing then increasing to the maximum value,
+	decreasing and increasing after the maximum value
+	*/
+
+}
---
+++ src/myPractice08_07/FindRepeatedChars.java
@@ -0,0 +1,41 @@
+package myPractice08_07;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class FindRepeatedChars {
+	
+	/*
+	Find the repeated characters in a String.
+	*/
+
+	public static void main(String[] args) {
+		
+		String str = "Shadi Ahmed";
+
+		findReapeatedChars(str);
+	}
+	
+	public static void findReapeatedChars(String str) {
+		
+		String arr[] = str.split("");
+		
+		Map<String, Integer> myMap = new HashMap();
+		
+		for(String each : arr) {
+			if(!myMap.containsKey(each)) {
+				myMap.put(each, 1);
+			}else {
+				myMap.put(each, myMap.get(each)+1);
+			}
+		}
+		for(Entry<String, Integer> w : myMap.entrySet()) {
+			if(w.getValue() > 1) {
+				System.out.println(w.getKey() + " is repeated " + w.getValue() + " times.");
+			}
+		}
+	}
+
+}
